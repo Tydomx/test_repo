a0232d09a6259f4004a320adc1038469f3a11e3a
Tydomx <michaelthai16@gmail.com>
2022-04-28 16:46:17 -0500

    test3

--- character.js
+++ character.js
@@ -1,4 +1,10 @@
 var car = "Access-Control-Allow-Origin: *";
+
+var requestOptions = {
+    headers: {
+        "Access-Control-Allow-Origin": "*"
+    }
+}
 // grabbing hero name and fetching api
 function fetching() {
     var publicKey = '70157e5fd5990a919a767f142118c129';
@@ -7,7 +13,8 @@ function fetching() {
     var charId = "1009368";
    //var url = `https://gateway.marvel.com/v1/public/characters${charId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
    var url = `https://gateway.marvel.com/v1/public/characters${charId}&apikey=${publicKey}`; 
-   fetch(url, { mode: "no-cors" }).then(function(response){
+   fetch(url, requestOptions)
+   .then(function(response){
         return response.json()
     })
     .then(function(data){
