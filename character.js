de4dd5f3a256f669beded8753a0b1bbea9c1d5bb
Tydomx <michaelthai16@gmail.com>
2022-04-28 16:43:35 -0500

    test

--- character.js
+++ character.js
@@ -1,3 +1,4 @@
+var car = "Access-Control-Allow-Origin: *";
 // grabbing hero name and fetching api
 function fetching() {
     var publicKey = '70157e5fd5990a919a767f142118c129';
@@ -6,7 +7,7 @@ function fetching() {
     var charId = "1009368";
    //var url = `https://gateway.marvel.com/v1/public/characters${charId}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
    var url = `https://gateway.marvel.com/v1/public/characters${charId}&apikey=${publicKey}`; 
-   fetch(url).then(function(response){
+   fetch(url, { mode: "no-cors" }).then(function(response){
         return response.json()
     })
     .then(function(data){
